e41c583280950ef386013bb945de38872b39cb28
aarchamb <anthony85180@gmail.com>
2016-04-27 11:22:20 +0200

    initial commit

---
+++ src/main/java/com/jmmv/back/JmmvApplication.java
@@ -0,0 +1,12 @@
+package com.jmmv.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmmvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmmvApplication.class, args);
+	}
+}
