7d3355f10934053537f2e38b2d4308b4bf3ee16b
aarchamb <anthony85180@gmail.com>
2016-04-29 14:41:11 +0100

    Rajouts d'attributs à l'entité Client

--- src/main/java/com/jmmv/back/Client.java
+++ src/main/java/com/jmmv/back/Client.java
@@ -1,5 +1,6 @@
 package com.jmmv.back;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.*;
@@ -19,36 +20,44 @@ public class Client {
     private  String prenom;
     private  String adresse;
     private  String telephone;
+    private  Boolean listeRouge;
     private  String fax;
     private  String email;
     private  String cp;
+    private  Date creation;
+    private  List<Date> relancesVaccin;
+    private  List<Date> relancesEMail;
+    private  Client clientRefere;
+    private  Client clientReferant;
     
     @OneToMany(mappedBy="client")
     private List<Animal> animaux;
-        
-    public Client(long id, String civilite, String nom, String prenom, String adresse, String telephone, String fax,
-			String email, String cp) {
+
+	public Client(long idClient, String civilite, String nom, String prenom, String adresse, String telephone,
+			Boolean listeRouge, String fax, String email, String cp, Date creation, List<Date> relancesVaccin,
+			List<Date> relancesEMail, Client clientRefere, Client clientReferant, List<Animal> animaux) {
 		super();
-		this.idClient = id;
+		this.idClient = idClient;
 		this.civilite = civilite;
 		this.nom = nom;
 		this.prenom = prenom;
 		this.adresse = adresse;
 		this.telephone = telephone;
+		this.listeRouge = listeRouge;
 		this.fax = fax;
 		this.email = email;
 		this.cp = cp;
+		this.creation = creation;
+		this.relancesVaccin = relancesVaccin;
+		this.relancesEMail = relancesEMail;
+		this.clientRefere = clientRefere;
+		this.clientReferant = clientReferant;
+		this.animaux = animaux;
+	}
+
+	public Client() {
 	}
     
-    public Client() {
-	}
-    
-	public long getId() {
-		return  idClient;
-	}
-	public void setId(long id) {
-		this.idClient = id;
-	}
 	public String getCivilite() {
 		return civilite;
 	}
@@ -96,6 +105,70 @@ public class Client {
 	}
 	public void setCp(String cp) {
 		this.cp = cp;
+	}
+	
+	public long getIdClient() {
+		return idClient;
+	}
+	
+	public void setIdClient(long idClient) {
+		this.idClient = idClient;
+	}
+	
+	public Boolean getListeRouge() {
+		return listeRouge;
+	}
+	
+	public void setListeRouge(Boolean listeRouge) {
+		this.listeRouge = listeRouge;
+	}
+	
+	public Date getCreation() {
+		return creation;
+	}
+	
+	public void setCreation(Date creation) {
+		this.creation = creation;
+	}
+	
+	public List<Date> getRelancesVaccin() {
+		return relancesVaccin;
+	}
+	
+	public void setRelancesVaccin(List<Date> relancesVaccin) {
+		this.relancesVaccin = relancesVaccin;
+	}
+	
+	public List<Date> getRelancesEMail() {
+		return relancesEMail;
+	}
+	
+	public void setRelancesEMail(List<Date> relancesEMail) {
+		this.relancesEMail = relancesEMail;
+	}
+	
+	public Client getClientRefere() {
+		return clientRefere;
+	}
+	
+	public void setClientRefere(Client clientRefere) {
+		this.clientRefere = clientRefere;
+	}
+	
+	public Client getClientReferant() {
+		return clientReferant;
+	}
+	
+	public void setClientReferant(Client clientReferant) {
+		this.clientReferant = clientReferant;
+	}
+	
+	public List<Animal> getAnimaux() {
+		return animaux;
+	}
+	
+	public void setAnimaux(List<Animal> animaux) {
+		this.animaux = animaux;
 	}
 	
 
